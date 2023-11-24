55eeaab6d4243d7cc73c0baaca239ef074276543
Mohammed Saqib Ahmed <MohammedSaqib.Ahmed@viatris.com>
2023-11-24 17:53:39 +0530

    adding a new project

---
+++ src/data.js
@@ -0,0 +1,39 @@
+const reviews = [
+    {
+        id:1,
+        name:"Pranay Gupta",
+        job: "Software Engineer",
+        image:"https://aeccc.targettechnology.in/static/media/PranayGupta.f3c530b7630ba8efb2ab.jpg",
+        text :"We enlisted to lead a critical project, and their software developmentexpertise played a key role in delivering a robust solution on time. Their code quality, problem-solving skills, and ability to collaborate with cross-functional teams were exceptional. is a true professional, and we look forward to future collaborations."
+    },
+    {
+        id:2,
+        name:"Abir Pal",
+        job: "Graphic Designer",
+        image:"https://aeccc.targettechnology.in/static/media/AbirPal.574a09ad7cb325853b29.jpg",
+        text :"We brought Designer's on board to elevate our design projects, and the results were outstanding. Their creativity, attention to design principles, and ability to capture our brand essence were truly impressive. Abir Pal has a unique talent for transforming ideas into visually stunning creations."
+    },
+    {
+        id:3,
+        name:"Soumya Banerjee",
+        job: "Marketing Manager",
+        image:"https://aeccc.targettechnology.in/static/media/SoumyaBanerjee.2e2521d6029842435080.jpg",
+        text :"Under leadership, our marketing strategy has reached new heights. Their strategic thinking, data-driven approach, and ability to execute effective campaigns have significantly contributed to our brand's success. Soumya Banerjee is a results-driven marketer with a keen understanding of the evolving market dynamics."
+    },
+    {
+        id:4,
+        name:"Saikat Mukherjee",
+        job: "Content Strategist",
+        image:"https://aeccc.targettechnology.in/static/media/PranayGupta.f3c530b7630ba8efb2ab.jpg",
+        text :"Mr Ahmed is the creative force behind our content strategy. Their ability to understand our brand, target audience, and market trends is unparalleled. [Content Strategist's Name] has not only shaped our content into compelling narratives but has also optimized our approach for maximum engagement. Their strategic thinking has elevated our brand storytelling, making an indispensable member of our creative team."
+    },
+    {
+        id:5,
+        name:"Aritra Biswas",
+        job: "DevOps Engineer",
+        image:"https://avatars.githubusercontent.com/u/93366359?v=4",
+        text :"Having  Mr Sadiq on our team has been a game-changer. Their expertise in DevOps has significantly streamlined our development and deployment processes. Sadiq brings a perfect blend of technical proficiency and strategic thinking. Our projects now run smoother, deployments are seamless, and downtime is a thing of the past.  is an invaluable asset to any tech team"
+    },
+];
+
+export default reviews;
